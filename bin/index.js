8955670d6e6788f87a01fe46f9b7e5b32a648b65
Zach Snow <z@zachsnow.com>
2016-05-13 16:16:56 -0700

    Clean up some error handling stuff.

--- bin/index.js
+++ bin/index.js
@@ -1,21 +1,26 @@
 (function(){
   var fs = require('fs');
   var yaml = require('js-yaml');
+  var _ = require('lodash');
   var process = require('process');
   var path = require('path');
   var DubloonsBot = require('../src/dubloons');
+  var domain = require('domain').create();
 
-  // Load configuration.
-  var filename = process.argv[2] || path.resolve(__dirname, '..', 'conf', 'config.yml');
-  try {
-    var options = yaml.safeLoad(fs.readFileSync(filename, 'utf8'));
-  }
-  catch(e){
+  var debug = _.indexOf(process.argv, '--debug') !== -1;
+
+  domain.on('error', function(e){
     console.error('dubloons: ' + e.message);
-    process.exit(-1);
-  }
+    if(debug){
+      throw e;
+    }
+  });
 
-  var bot = new DubloonsBot(options);
-  bot.run();
+  domain.run(function(){
+    var filename = process.argv[2] || path.resolve(__dirname, '..', 'conf', 'config.yml');
+    var options = yaml.safeLoad(fs.readFileSync(filename, 'utf8'));
+    var bot = new DubloonsBot(options);
+    bot.run();
+  });
 
 })();
