8cd62e628610ede724555f1afcf6e883b158d703
Zach Snow <z@zachsnow.com>
2016-05-13 17:31:18 -0700

    Fix domain typos; thread debug option through.

--- bin/index.js
+++ bin/index.js
@@ -1,24 +1,28 @@
 (function(){
+  var domain = require('domain');
   var fs = require('fs');
   var yaml = require('js-yaml');
   var _ = require('lodash');
   var process = require('process');
   var path = require('path');
+  var Botkit = require('botkit');
   var DubloonsBot = require('../src/dubloons');
-  var domain = require('domain').create();
+
+  var dubloonsDomain = domain.create();
 
   var debug = _.indexOf(process.argv, '--debug') !== -1;
 
-  domain.on('error', function(e){
+  dubloonsDomain.on('error', function(e){
     console.error('dubloons: ' + e.message);
     if(debug){
       throw e;
     }
   });
 
-  domain.run(function(){
+  dubloonsDomain.run(function(){
     var filename = process.argv[2] || path.resolve(__dirname, '..', 'conf', 'config.yml');
     var options = yaml.safeLoad(fs.readFileSync(filename, 'utf8'));
+    options.debug = debug;
     var bot = new DubloonsBot(options);
     bot.run();
   });
